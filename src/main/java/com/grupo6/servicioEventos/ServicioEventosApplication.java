5b6c59d4d14b3790ef4b9a3cbd839b4d394aa27e
Esteban <esteban.morauc@gmail.com>
2025-05-19 20:14:25 -0400

    Nuevo Microservicio

---
+++ src/main/java/com/grupo6/servicioEventos/ServicioEventosApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/servicioEventos/ServicioEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
